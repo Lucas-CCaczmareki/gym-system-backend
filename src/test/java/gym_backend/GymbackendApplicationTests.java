e7a723e5281fd6f29777d31eadea6edd76c8eec1
Lucas-CCaczmareki <lucascaczmareki@gmail.com>
2025-12-27 19:36:04 -0300

    first commit

---
+++ src/test/java/gym_backend/GymbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package gym_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
